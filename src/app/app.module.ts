9082456bcf9e4493cce1bcb16d5f1e18780dda52
Álvaro Alejandro Zarabanda Gutiérrez <aazarabandag@correo.udistrital.edu.co>
2022-06-20 13:47:24 -0500

    se crean los componentes para header y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './componentes/componente_creacion_de_proyecto/app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
