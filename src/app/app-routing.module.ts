3aced24dc9462cf9207e1ee1818e9e37801e2d1d
aalejoz25 <alvaroalejo25@gmail.com>
2022-06-21 13:15:09 -0500

    se agrega funcionalidad de boton login e inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './componentes/inicio/inicio.component'
+import { LoginComponent } from './componentes/login/login.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: 'inicio', component: InicioComponent },
-  { path:'**',pathMatch:'full',redirectTo:'inicio'}
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' },
+  //{ path: '', redirectTo: 'inicio' },
+  
+  
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash:true})],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
