2a2894abef4e4aeb3708594e0cb82cecb48a22f4
aalejoz25 <alvaroalejo25@gmail.com>
2022-07-23 21:43:33 -0500

    update server conf

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/scigrow-web'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/myapp/index.html'));});
+'/dist/scigrow-web/index.html'));});
 app.listen(process.env.PORT || 8080);
