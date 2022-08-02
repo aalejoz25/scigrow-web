885b7848e60b5b0dc59adcbcadb420f9d1dabe05
aalejoz25 <alvaroalejo25@gmail.com>
2022-08-02 07:43:36 -0500

    se crea api service y se eliminan paginas sin uso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { PoliticasPrivacidadComponent } from './componentes/politicas-privacidad/politicas-privacidad.component'
 import { LoginComponent } from './componentes/login/login.component';
 import { BusquedasComponent } from './componentes/busquedas/busquedas.component';
-import { RegistroComponent } from './componentes/registro/registro.component';
 import { VistaProyectosUsuarioComponent } from './componentes/vista-proyectos-usuario/vista-proyectos-usuario.component';
 import { ContrasenaComponent } from './componentes/contrasena/contrasena.component';
 import { AdicionarComponent } from './componentes/adicionar/adicionar.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+
+  { path: 'inicio', component: LoginComponent },
   { path: 'politicas_privacidad', component: PoliticasPrivacidadComponent },
-  { path: 'registro', component: RegistroComponent },
   { path: 'proyectos_usuario', component: VistaProyectosUsuarioComponent },
   { path: 'busquedas', component: BusquedasComponent },
   { path: 'contrasena', component: ContrasenaComponent },
-  {path:'adicionar', component: AdicionarComponent},
-  { path: '**', pathMatch: 'full', redirectTo: 'login' },
-
-  //{ path: '', redirectTo: 'inicio' },
+  { path: 'adicionar', component: AdicionarComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' },
+  { path: '', component: LoginComponent},
 ];
 
 @NgModule({
