0cd012a6dd8e26c04cdea89e827a961b953bf596
aalejoz25 <alvaroalejo25@gmail.com>
2022-08-16 14:18:48 -0500

    se agrega servicio de sesion

---
+++ src/app/interfaces/usuario.interface.ts
@@ -0,0 +1,10 @@
+export interface Usuario{
+    isLogged: string,
+    userType: string,
+    id: string,
+    userName: string,
+    pass: string,
+    email: string,
+    fName: string,
+    lName: string
+}
